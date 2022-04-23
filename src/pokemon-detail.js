8a071eb933e00304941c090483651d77a5dc4a69
Ayush Katoch <katochayushhp@gmail.com>
2022-04-23 17:28:23 +0530

    Update pokemon-detail.js

--- src/pokemon-detail.js
+++ src/pokemon-detail.js
@@ -1,50 +1,15 @@
 import React from "react";
+import { DelaySpinner } from "./ui";
 
-import { fetchPokemon, suspensify } from "./api";
-
-let initialPokemon = suspensify(fetchPokemon(1));
-
-function DelaySpinner() {
-  return (
-    <span role="img" aria-label="spinner" className="DelaySpinner">
-      <style>{`
-        .DelaySpinner {
-          animation: rotation 1.5s infinite linear;
-          display: inline-block;
-          font-size: .7rem
-        }
-
-        @keyframes rotation {
-          from { transform: rotate(0deg) }
-          to { transform: rotate(359deg) }
-        }
-      `}</style>
-      🌀
-    </span>
-  );
-}
-
-export default function PokemonDetail() {
-  let [pokemonResource, setPokemonResource] = React.useState(initialPokemon);
-  const [isPending, startTransition] = React.useTransition({ timeoutMs: 3000});
-let pokemon = pokemonResource.read();
+export default function PokemonDetail({ resource, isStale }) {
+  let pokemon = resource.read();
 
   return (
     <div>
-      <div>{pokemon.name} {isPending && <DelaySpinner />}</div>
-
-      <button
-        type="button"
-        // Dont want to use the button again until the next pokemon comes
-        disabled={isPending}
-        onClick={() =>
-          startTransition(() =>
-            setPokemonResource(suspensify(fetchPokemon(pokemon.id + 1)))
-          )
-        }
-      >
-        Next 
-      </button>
+      <div style={isStale ? { opacity: 0.5 } : null}>
+        {pokemon.name}
+        {isStale && <DelaySpinner />}
+      </div>
     </div>
   );
 }
